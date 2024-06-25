e8cfac699ba7f8731369d95bcfa50a8654915f9e
Esha Shah <esha.shah@tatvasoft.com>
2024-06-25 13:57:45 +0530

    Base architecture

---
+++ MauiControlsSample/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using MauiControlsSample.ViewModels;
+
+namespace MauiControlsSample
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage(MainPageViewModel viewModel)
+        {
+            InitializeComponent();
+            BindingContext = _viewModel = viewModel;
+        }
+
+        private MainPageViewModel _viewModel;
+    }
+}
---
+++ MauiControlsSample/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,20 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.ObjectModel;
+using MauiControlsSample.Models;
+
+namespace MauiControlsSample.ViewModels
+{
+    public partial class MainPageViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private ObservableCollection<Item> items;
+
+        public MainPageViewModel()
+        {
+            Items = new ObservableCollection<Item>
+            {
+                new Item { Name = "Shell", Description = "The visual hierarchy of the app is described in a class that subclasses the Shell class." }
+            };
+        }
+    }
+}
